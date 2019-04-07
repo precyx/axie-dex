e78f5a9a1f4b4096621a9c41ee9b102bb1df5e9b
pquantum <pr.pascal.bachmann@gmail.com>
2019-04-07 02:03:46 +0200

    add useOnClickOutside hook

--- src/components/ui/MenuToggle.tsx
+++ src/components/ui/MenuToggle.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, {useRef} from "react";
 import {useToggle} from "./Toggle/useToggle";
+import {useOnClickOutside} from "../../hooks/useOnClickOutside";
 
 
 interface MenuToggleProps {
@@ -13,8 +14,12 @@ export const MenuToggle = (props:MenuToggleProps):JSX.Element => {
 
 	const [isOn, toggle] = useToggle(props.isOn || false);
 
+	const ref:any = useRef<HTMLDivElement>(null);
+
+	useOnClickOutside(ref, () => {toggle(false)});
+
 	return (
-		<div onClick={toggle} className={props.className}>
+		<div ref={ref} onClick={toggle} className={props.className}>
 			{props.children}
 			{props.render(isOn)}
 		</div>
--- src/components/ui/Toggle/useToggle.tsx
+++ src/components/ui/Toggle/useToggle.tsx
@@ -1,10 +1,12 @@
-
 import React, {useState} from "react";
 
 export function useToggle (initialState:boolean){
 
 	const [state, setState] = useState(initialState);
-	const toggle:any = () => setState(state => !state);
+	const toggle:any = (forcedState?:boolean) => {
+		if(forcedState === true || forcedState === false) setState(forcedState);
+		else setState(state => !state);
+	}
 
 	return [state, toggle];
 }
